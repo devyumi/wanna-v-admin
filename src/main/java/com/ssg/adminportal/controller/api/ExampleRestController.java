383172ba611eefe15013f9d22fc52bd204c198e5
LeeJiEun <gsilverg197@gmail.com>
2024-11-05 09:32:40 +0900

    🎉 Init: set up initial project structure

---
+++ src/main/java/com/ssg/adminportal/controller/api/ExampleRestController.java
@@ -0,0 +1,5 @@
+package com.ssg.adminportal.controller.api;
+
+public class ExampleRestController {
+
+}
---
+++ src/main/java/com/ssg/adminportal/controller/web/ExampleController.java
@@ -0,0 +1,5 @@
+package com.ssg.adminportal.controller.web;
+
+public class ExampleController {
+
+}
