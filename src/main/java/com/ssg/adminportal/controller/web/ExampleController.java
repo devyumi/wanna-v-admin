f734f42fde3ba3095352ffebb4a97e19013b14e1
nam3280 <nam3280@gmail.com>
2024-11-12 18:33:24 +0900

    🎉Init :파일, 패키지명 변경

--- src/main/java/com/ssg/adminportal/controller/web/ExampleController.java
+++ src/main/java/com/ssg/adminportal/controller/web/ExampleController.java
@@ -1,5 +1,15 @@
 package com.ssg.adminportal.controller.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/")
 public class ExampleController {
 
+    @GetMapping("/get")
+    public String test(){
+        return "/admin/index";
+    }
 }
