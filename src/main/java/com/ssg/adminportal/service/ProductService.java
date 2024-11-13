3a9e93d780be3a5145ef98482ece77c8b7b774e1
LeeJiEun <gsilverg197@gmail.com>
2024-11-13 15:11:49 +0900

    ✨ Feat: implement product list retrieval in service layer

---
+++ src/main/java/com/ssg/adminportal/service/ProductService.java
@@ -0,0 +1,11 @@
+package com.ssg.adminportal.service;
+
+import com.ssg.adminportal.domain.Product;
+import com.ssg.adminportal.dto.response.ProductResponseDTO;
+import java.util.List;
+
+public interface ProductService {
+
+    List<ProductResponseDTO> getProductList();
+
+}
