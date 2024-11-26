3559ea366a4c624f96de297e93f89335326154a6
devyumi <yyumiwing@naver.com>
2024-11-26 13:29:45 +0900

    feat: 리뷰 태그 전체 조회 컨트롤러 추가

---
+++ src/main/java/com/ssg/adminportal/controller/web/TagController.java
@@ -0,0 +1,28 @@
+package com.ssg.adminportal.controller.web;
+
+import com.ssg.adminportal.dto.TagSaveDTO;
+import com.ssg.adminportal.dto.request.TagRequestDTO;
+import com.ssg.adminportal.service.TagService;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequiredArgsConstructor
+@RequestMapping("/reviews/tags")
+@Slf4j
+public class TagController {
+
+    private final TagService tagService;
+
+    @GetMapping("reviews/tags")
+    public String getReviewTags(TagRequestDTO tagRequestDTO, Model model) {
+        model.addAttribute("tags", tagService.findTags(tagRequestDTO));
+        model.addAttribute("tagSaveDTO", new TagSaveDTO());
+        model.addAttribute("tagUpdateDTO", new TagSaveDTO());
+        return "review/tag/tags";
+    }
+}
