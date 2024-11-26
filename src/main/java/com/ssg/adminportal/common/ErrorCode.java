8c9eb36ef5af1c4406a0d455d7ac40a6104106bc
LeeJiEun <gsilverg197@gmail.com>
2024-11-27 01:02:45 +0900

    ✨ Feat: Statistic 관련 Error code 추가

--- src/main/java/com/ssg/adminportal/common/ErrorCode.java
+++ src/main/java/com/ssg/adminportal/common/ErrorCode.java
@@ -2,7 +2,9 @@ package com.ssg.adminportal.common;
 
 public enum ErrorCode {
     INVALID_VALUE(400, "Invalid value provided"),
-    NON_EXISTENT_ID(404, "The ID does not exist");
+    NON_EXISTENT_ID(404, "The ID does not exist"),
+    INVALID_REQUEST_TYPE(400, "Invalid request type provided"),
+    DATE_CALCULATION_ERROR(500, "An error occurred while calculating the date range");
 
     private int code;
     private String message;
