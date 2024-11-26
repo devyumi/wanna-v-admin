08d79f83682559e76a32c1264c40af8ddd50c649
devyumi <yyumiwing@naver.com>
2024-11-26 13:17:11 +0900

    feat: 리뷰 상세 조회 컨트롤러 추가

--- src/main/java/com/ssg/adminportal/controller/web/ReviewController.java
+++ src/main/java/com/ssg/adminportal/controller/web/ReviewController.java
@@ -1,17 +1,19 @@
 package com.ssg.adminportal.controller.web;
 
 import com.ssg.adminportal.dto.request.ReviewRequestDTO;
+import com.ssg.adminportal.dto.request.ReviewUpdateStatusDTO;
 import com.ssg.adminportal.service.ReviewService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 @RequiredArgsConstructor
-@RequestMapping("reviews")
+@RequestMapping("/reviews")
 @Slf4j
 public class ReviewController {
 
@@ -21,5 +23,12 @@ public class ReviewController {
     public String getReviews(ReviewRequestDTO reviewRequestDTO, Model model) {
         model.addAttribute("reviews", reviewService.findReviews(reviewRequestDTO));
         return "review/reviews";
+    }
+
+    @GetMapping("/{id}")
+    public String getReview(@PathVariable Long id, Model model) {
+        model.addAttribute("review", reviewService.findReview(id));
+        model.addAttribute("reviewUpdateStatusDTO", new ReviewUpdateStatusDTO());
+        return "review/review";
     }
 }
