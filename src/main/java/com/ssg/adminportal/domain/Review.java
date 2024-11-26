10a8caf713fea569a8aa509f62e877cd0334f4e0
devyumi <yyumiwing@naver.com>
2024-11-26 13:13:59 +0900

    refactor: Review domain 리팩토링

--- src/main/java/com/ssg/adminportal/domain/Review.java
+++ src/main/java/com/ssg/adminportal/domain/Review.java
@@ -8,6 +8,7 @@ import lombok.NoArgsConstructor;
 
 import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.util.List;
 
 @Data
 @NoArgsConstructor
@@ -34,14 +35,29 @@ public class Review {
     private String image;
 
     @Temporal(TemporalType.DATE)
+    @Column(name = "visit_date")
     private LocalDate visitDate;
 
     @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
     private LocalDateTime createdAt;
 
     @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
     private LocalDateTime updatedAt;
 
+    @Column(name = "is_active")
     private Boolean isActive;
     private String note;
+
+    @OneToMany(mappedBy = "review")
+    private List<ReviewTag> reviewTags;
+
+    /**
+     * 연관관계 편의 메서드
+     */
+    public void addRestaurant(Restaurant restaurant) {
+        this.restaurant = restaurant;
+        restaurant.getReviews().add(this);
+    }
 }
