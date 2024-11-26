0020b838d156e3aa6765dd3c12d3d2c9d90d56de
devyumi <yyumiwing@naver.com>
2024-11-26 13:30:23 +0900

    style: 중복 코드 제거

--- src/main/java/com/ssg/adminportal/controller/web/TagController.java
+++ src/main/java/com/ssg/adminportal/controller/web/TagController.java
@@ -18,7 +18,7 @@ public class TagController {
 
     private final TagService tagService;
 
-    @GetMapping("reviews/tags")
+    @GetMapping
     public String getReviewTags(TagRequestDTO tagRequestDTO, Model model) {
         model.addAttribute("tags", tagService.findTags(tagRequestDTO));
         model.addAttribute("tagSaveDTO", new TagSaveDTO());
