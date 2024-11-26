57aed72d6f5b96e7aebe7a93be9ee07bd7fd535b
LeeJiEun <gsilverg197@gmail.com>
2024-11-26 17:31:00 +0900

    ✨ Feat: StatisticRestController api 설계

---
+++ src/main/java/com/ssg/adminportal/controller/api/StatisticRestController.java
@@ -0,0 +1,47 @@
+package com.ssg.adminportal.controller.api;
+
+import com.ssg.adminportal.dto.StatisticDTO;
+import com.ssg.adminportal.service.StatisticService;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Log4j2
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/api/statistics")
+public class StatisticRestController {
+
+    private final StatisticService statisticService;
+
+    // 월별 통계 API
+    @GetMapping("/monthly")
+    public ResponseEntity<Map<String, Object>> getMonthlyStatistics() {
+        List<StatisticDTO> monthlyStatistics = statisticService.getMonthlyStatistics();
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "success");
+        response.put("data", monthlyStatistics);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    // 주별 통계 API
+    @GetMapping("/weekly")
+    public ResponseEntity<Map<String, Object>>  getWeeklyStatistics() {
+        List<StatisticDTO> weeklyStatistics = statisticService.getWeeklyStatistics();
+
+        Map<String, Object> response = new HashMap<>();
+        response.put("status", "success");
+        response.put("data", weeklyStatistics);
+
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+}
