74cbbdec3a535e98e283efb09f531c35e6c00822
LeeJiEun <gsilverg197@gmail.com>
2024-11-26 17:30:04 +0900

    ✨ Feat: 대시보드에 노출 시키는 월별/주별 통계 쿼리 작성

--- src/main/java/com/ssg/adminportal/mapper/StatisticMapper.java
+++ src/main/java/com/ssg/adminportal/mapper/StatisticMapper.java
@@ -1,5 +1,7 @@
 package com.ssg.adminportal.mapper;
 
+import com.ssg.adminportal.dto.StatisticDTO;
+import java.util.List;
 import org.apache.ibatis.annotations.Mapper;
 
 @Mapper
@@ -8,4 +10,8 @@ public interface StatisticMapper {
     void insertMonthlyStatistics();
 
     void insertWeeklyStatistics();
+
+    List<StatisticDTO> getMonthlyStatistics();
+
+    List<StatisticDTO> getWeeklyStatistics();
 }
