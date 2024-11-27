4955f23333a0751f729ca54bff3ef55d30b17493
LeeJiEun <gsilverg197@gmail.com>
2024-11-27 16:15:55 +0900

    ✨ Feat: NCP Clova Studio 감정 분석을 통한 리뷰 감정 분석 API 구현

--- src/main/java/com/ssg/adminportal/service/ReviewService.java
+++ src/main/java/com/ssg/adminportal/service/ReviewService.java
@@ -12,4 +12,5 @@ public interface ReviewService {
     Review findReview(Long reviewId);
     void updateReviewActiveFalse(Long reviewId, String note);
     void updateReviewActiveTrue(Long reviewId);
+    void updateReviewSentiment();
 }
