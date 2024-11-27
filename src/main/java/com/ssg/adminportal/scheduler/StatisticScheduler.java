c68655a8a5a7b820117ddc8ee928f8e29aca342a
LeeJiEun <gsilverg197@gmail.com>
2024-11-27 16:16:30 +0900

    ✨ Feat: 매일 새벽 00시 30분 작일 분석 쿼리 실행 Scheduled 구현

--- src/main/java/com/ssg/adminportal/scheduler/StatisticScheduler.java
+++ src/main/java/com/ssg/adminportal/scheduler/StatisticScheduler.java
@@ -1,6 +1,7 @@
 package com.ssg.adminportal.scheduler;
 
 import com.ssg.adminportal.mapper.StatisticMapper;
+import com.ssg.adminportal.service.ReviewService;
 import java.time.LocalDateTime;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -13,8 +14,10 @@ import org.springframework.stereotype.Component;
 public class StatisticScheduler {
 
     private final StatisticMapper statisticMapper;
+    private final ReviewService reviewService;
 
     /**
+     * 대시보드 월별 통계
      * 매월 1일 새벽 2시 지난달 통계 쿼리 실행
      */
     @Scheduled(cron = "0 0 2 1 * ?")
@@ -25,6 +28,7 @@ public class StatisticScheduler {
     }
 
      /**
+     * 대시보드 주별 통계
      * 매주 월요일 새벽 2시 지난달 통계 쿼리 실행
      */
     @Scheduled(cron = "0 0 2 * * MON")
@@ -32,5 +36,16 @@ public class StatisticScheduler {
         log.info(LocalDateTime.now() + " 주별 통계 업데이트 시작");
         statisticMapper.insertWeeklyStatistics();
         log.info(LocalDateTime.now() + " 주별 통계 업데이트 종료");
+    }
+
+    /**
+     * 식당 리뷰 감정 분석
+     * 매일 새벽 00시 30분 작일 분석 쿼리 실행
+     */
+    @Scheduled(cron = "0 30 0 * * *")
+    public void updateReviewSentiment() {
+        log.info(LocalDateTime.now() + " 리뷰 감정 분석 시작");
+        reviewService.updateReviewSentiment();
+        log.info(LocalDateTime.now() + " 리뷰 감정 분석 종료");
     }
 }
