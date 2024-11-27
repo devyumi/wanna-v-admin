78fea35acca0119fbbec506ab26d9de6b7ddec83
LeeJiEun <gsilverg197@gmail.com>
2024-11-27 16:13:20 +0900

    ✨ Feat: 감정 분석 관련 ErrorCode 추가

--- src/main/java/com/ssg/adminportal/common/ErrorCode.java
+++ src/main/java/com/ssg/adminportal/common/ErrorCode.java
@@ -4,7 +4,10 @@ public enum ErrorCode {
     INVALID_VALUE(400, "Invalid value provided"),
     NON_EXISTENT_ID(404, "The ID does not exist"),
     INVALID_REQUEST_TYPE(400, "Invalid request type provided"),
-    DATE_CALCULATION_ERROR(500, "An error occurred while calculating the date range");
+    DATE_CALCULATION_ERROR(500, "An error occurred while calculating the date range"),
+    SENTIMENT_ANALYSIS_FAILURE(500, "An review sentiment analysis failed"),
+    CLOVA_API_CLIENT_ERROR(400, "Clova Studio API returned a client error"),
+    CLOVA_API_SERVER_ERROR(500, "Clova Studio API returned a server error");
 
     private int code;
     private String message;
