06ae8318c233fb49fd5a81230ddca0776376659d
nam3280 <nam3280@gmail.com>
2024-12-04 17:40:52 +0900

    유저 쿼리문 수정

--- src/main/java/com/ssg/adminportal/repository/UserGradeLogRepository.java
+++ src/main/java/com/ssg/adminportal/repository/UserGradeLogRepository.java
@@ -2,10 +2,11 @@ package com.ssg.adminportal.repository;
 
 import com.ssg.adminportal.domain.UserGradeLog;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface UserGradeLogRepository extends JpaRepository<UserGradeLog, Long> {
-
+    @Query("SELECT u FROM UserGradeLog u WHERE u.id = :userId ORDER BY u.updateAt DESC")
     UserGradeLog findByUserId(Long userId);
 }
